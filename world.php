b47e2c8f605485336890f3bd1fb54d15207872fa
Kelly Christian <ms.kchristian86@gmail.com>
2022-11-17 23:26:38 -0400

    edited php table

--- world.php
+++ world.php
@@ -1,17 +1,34 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+
+$country =$_GET['country'];
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+echo "<table>
+<tr>
+<th> Country Name </th>
+<th> Continent</th>
+<th> Independence </th>
+<th> Head of State </th>
+</tr>";
+
+
 ?>
-<ul>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  {
+    echo "<tr>";
+    echo "<td>".$row['name']."</td>";
+    echo "<td>".$row['continent']."</td>";
+    echo "<td>".$row['independence_year']."</td>";
+    echo "<td>".$row['head_of_state']."</td>";
+    echo "</tr>";
+  }
 <?php endforeach; ?>
-</ul>
+echo "</table>";
